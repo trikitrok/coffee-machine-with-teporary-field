6051721c138cb1109d0e60628e272ce69309a67c
trikitrok <trikitrok@gmail.com>
2024-02-27 23:56:25 +0100

    extract OrderProcessing to a separate file

--- src/coffee-machine.ts
+++ src/coffee-machine.ts
@@ -1,6 +1,6 @@
 import {Drink} from "./drink";
 import {DrinkMakerDriver} from "./drink-maker-driver";
-import {Order} from "./order";
+import {OrderProcessing} from "./order-processing";
 
 export class CoffeeMachine {
     private orderProcessing: OrderProcessing;
@@ -68,60 +68,3 @@ export class CoffeeMachine {
     }
 }
 
-class OrderProcessing {
-    private readonly MAX_SPOONS_OF_SUGAR: number = 2;
-    private selectedDrink: Drink;
-    private extraHot: boolean;
-    private spoonsOfSugars: number;
-    private money: number;
-    private readonly priceTable: Record<Drink, number>;
-
-    constructor(priceTable: Record<Drink, number>) {
-        this.priceTable = priceTable;
-        this.money = 0;
-        this.spoonsOfSugars = 0;
-        this.extraHot = false;
-    }
-
-    isOrderReady(): boolean {
-        return this.selectedDrink !== undefined;
-    }
-
-    selectDrink(selectedDrink: Drink): void {
-        this.selectedDrink = selectedDrink;
-    }
-
-    isExtraHot(): void {
-        if (this.selectedDrink === Drink.OrangeJuice) {
-            return;
-        }
-        this.extraHot = true;
-    }
-
-    addOneSpoonOfSugar(): void {
-        this.spoonsOfSugars = Math.min(
-            this.spoonsOfSugars + 1,
-            this.MAX_SPOONS_OF_SUGAR
-        );
-    }
-
-    createOrder(): Order {
-        return new Order(this.selectedDrink, this.extraHot, this.spoonsOfSugars);
-    }
-
-    addMoney(amount: number): void {
-        this.money += amount;
-    }
-
-    computeMissingMoney(): number {
-        return this.getSelectedDrinkPrice() - this.money;
-    }
-
-    isThereEnoughMoney(): boolean {
-        return this.money >= this.getSelectedDrinkPrice();
-    }
-
-    private getSelectedDrinkPrice(): number {
-        return this.priceTable[this.selectedDrink];
-    }
-}
